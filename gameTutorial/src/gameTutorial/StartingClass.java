4ecdd94359e58301f58a777e44f796a597f4f95f
james.wilson <james.wilson@BFS-PRODUCT-19.verint.corp.verintsystems.com>
2015-07-15 12:19:52 +0100

    Adding Character

--- gameTutorial/src/gameTutorial/StartingClass.java
+++ gameTutorial/src/gameTutorial/StartingClass.java
@@ -3,24 +3,42 @@ package gameTutorial;
 import java.applet.Applet;
 import java.awt.Color;
 import java.awt.Frame;
+import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.net.URL;
 
 public class StartingClass extends Applet implements Runnable, KeyListener {
-
+	
+	private Player player;
+	private Image image, character;
+	private Graphics second;
+	private URL base;
+	
     @Override
     public void init() {
-
         setSize(800, 480);
         setBackground(Color.BLACK);
         setFocusable(true);
         addKeyListener(this);
         Frame frame = (Frame) this.getParent().getParent();
         frame.setTitle("Game Tutorial");
+        
+        try {
+			base = getDocumentBase();
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+
+		// Image Setups
+		character = getImage(base, "data/character.png");
     }
 
     @Override
     public void start() {
+    	player = new Player();
+    	
         Thread thread = new Thread(this);
         thread.start();
     }
@@ -38,7 +56,6 @@ public class StartingClass extends Applet implements Runnable, KeyListener {
     @Override
     public void run() {
         while (true) {
-
             repaint();
             try {
                 Thread.sleep(17);
@@ -47,6 +64,27 @@ public class StartingClass extends Applet implements Runnable, KeyListener {
             }
         }
     }
+    
+    @Override
+	public void update(Graphics g) {
+		if (image == null) {
+			image = createImage(this.getWidth(), this.getHeight());
+			second = image.getGraphics();
+		}
+		second.setColor(getBackground());
+		second.fillRect(0, 0, getWidth(), getHeight());
+		second.setColor(getForeground());
+		paint(second);
+
+		g.drawImage(image, 0, 0, this);
+
+	}
+
+	@Override
+	public void paint(Graphics g) {
+		g.drawImage(character, player.getCenterX() - 61, player.getCenterY() - 63, this);
+
+	}
 
     @Override
     public void keyPressed(KeyEvent e) {
